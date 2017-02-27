48e302b7e2cf13626060e292e0b654995e21c284
Jan Humphries <jldennison@gmail.com>
2017-02-27 16:21:14 -0500

    pseudocode out plan to approach problem

--- app/app/models/generator.rb
+++ app/app/models/generator.rb
@@ -1,4 +1,27 @@
 class Generator < ApplicationRecord
   validates :start_param, presence: true, numericality: { only_integer: true }
   validates :end_param, presence: true, numericality: { only_integer: true }
+
+  # pseudocode the algorithm
+  # take in two integers
+  # return any prime numbers in between, inclusive
+  # first step: strip any even numbers
+
+  # brute force:
+  # for each number n btwn both integers, do
+  # test number x in range 3..n
+  # divisible by x not prime, end
+  # not divisible, is prime, push to list
+  # test every odd number from 3 to n
+  # could get to be whoa long method
+
+  # recursive:
+  # starting at x = 3, check for modulus == 0
+  # if true, add number to placeholder array
+  # if not, add 2 to x and recursively call the number again
+
+
+  # def prime_factors(start, end)
+  #
+  # end
 end
