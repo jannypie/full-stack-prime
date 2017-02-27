162c37b00efc41c91ae251c959e947188a6f610d
Jan Humphries <jldennison@gmail.com>
2017-02-27 18:01:57 -0500

    give prime method a fresh start

--- app/app/models/generator.rb
+++ app/app/models/generator.rb
@@ -2,34 +2,18 @@ class Generator < ApplicationRecord
   validates :start_param, presence: true, numericality: { only_integer: true }
   validates :end_param, presence: true, numericality: { only_integer: true }
 
-  # pseudocode the algorithm
-  # take in two integers
-  # return any prime numbers in between, inclusive
+  # new pseudocode
+  # rethinking my recursive method
+  # needs: input, function, breakpoint, output
+  # if 2..7, input 7
+  # 7/7 = 1
+  # breakpoint is when all numbers have been tested
+  # and the only value is one that returns 1
 
-  # brute force:
-  # for each number n btwn both integers, do
-  # test number x in range 3..n
-  # divisible by x not prime, end
-  # not divisible, is prime, push to list
-  # test every odd number from 3 to n
-  # could get to be whoa long method
-
-  # recursive:
-  # starting at x = 3, check for modulus == 0
-  # if true, add number to placeholder array
-  # if not, add 2 to x and recursively call the number again
-
-
-  def make_range
-    # generate range between numbers
-    return Range.new(self.start_param,self.end_param)
-  end
-
-  # decided to skip removing all evens, test against 2..n
   def primes(n)
     if n <= 1
       return []
     end
-
   end
+  
 end
