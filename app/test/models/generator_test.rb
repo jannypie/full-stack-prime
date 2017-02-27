85bfff230d86cd39cefcf653e0ae62c8bd908d11
Jan Humphries <jldennison@gmail.com>
2017-02-27 15:01:45 -0500

    dev and tests for required params

--- app/test/models/generator_test.rb
+++ app/test/models/generator_test.rb
@@ -5,8 +5,34 @@ class GeneratorTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "should not save without starting param integer" do
+  test "should not save without parameters" do
     generator = Generator.new
+    assert_not generator.save, "Created the generator without parameters"
+  end
+
+  test "should not save without starting param integer" do
+    end_param = {
+      :end_param => 123
+    }
+    generator = Generator.new(end_param)
     assert_not generator.save, "Created the generator without start param"
   end
+
+  test "should not save without ending param integer" do
+    start_param = {
+      :start_param => 123
+    }
+    generator = Generator.new(start_param)
+    assert_not generator.save, "Created the generator without ending param"
+  end
+
+  test "should save with start and end parameters" do
+    params = {
+      :start_param => 1,
+      :end_param => 123
+    }
+    generator = Generator.new(params)
+    assert generator.save, "Created the generator without params"
+  end
+
 end
